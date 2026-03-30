c06798380c159c06c1c66a5e9ece3fb9ad3bbd95
Naiomi Sutphen <149907005+naiomisut@users.noreply.github.com>
2026-03-29 22:05:05 -0700

    CSE 445 Project 4 (phase 1)

---
+++ submission.cs
@@ -0,0 +1,68 @@
+using System;
+using Sytstem.Xml.Schema;
+using System.Xml;
+using System.Io;
+using Newtonsoft.json;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    public class Submission
+    {
+        // github page url 1
+        public static string xmlURL = "";
+        // github page url 2
+        public static string xsdURL = "";
+        // github page url 3
+        public static string smlErrorURL = "";
+
+        public static void Main(string[] args)
+        {
+            string res = Verification(xmlURL, xsdURL);
+            ConsoleApp1.WriteLine(res);
+            res = Verification(xmlErrorURL, xsdURL);
+            ConsoleApp1.WriteLine(res);
+            res = Xm12Json(xmlURL);
+            ConsoleApp1.WriteLine(res);
+
+        }
+        public static string Verification(string xmlURL, string xsdURL)
+        {
+            try
+            {
+                XmlScehmaSet schemas = new XmlScehmaSet();
+                schemas.Add("", xmlUrl);
+                XmlReaderSettings sets = new XmlReaderSettings();
+                sets.ValidationType = ValidationType.Schema;
+                List<string> errors = new List<string>();
+                sets.ValidationEventHandler += (sender, errors) => errors.Add(errors.Message);
+                using (XmlReader reader = XmlReader.Create(xmlURL, sets))
+                {
+                    while (reader.Read()) { }
+                }
+                return errors.Count == 0 ? "No errors are found" :
+                string.Join("\n", errors);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+
+        }
+        public static string Xml12Json(string xmlUrl)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlUrl);
+                string jsontext = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
+                return jsontext;
+            }
+            catch (Exception e)
+            {
+                return ex.Message;
+            }
+        }
+    }
+
+}
