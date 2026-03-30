14b58e5706668212fab53cd79e8f15abd6aa550a
Naiomi Sutphen <149907005+naiomisut@users.noreply.github.com>
2026-03-29 22:36:16 -0700

    Phase(5)

--- submission.cs
+++ submission.cs
@@ -4,69 +4,58 @@ using System.Xml;
 using System.IO;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+
 namespace ConsoleApp1
 {
     public class Submission
     {
-        // github page url 1
-        public static string xmlURL = "https://raw.githubusercontent.com/naiomisut/Assignment-445-4/main/NationalParks.xml";
-        // github page url 2
-        public static string xsdURL = "https://raw.githubusercontent.com/naiomisut/Assignment-445-4/main/NationalParks.xsd";
-        // github page url 3
-        public static string xmlErrorURL = "https://raw.githubusercontent.com/naiomisut/Assignment-445-4/main/NationalParksErrors.xml";
+        public static string xmlURL = "RAW_XML_URL";
+        public static string xmlErrorURL = "RAW_ERROR_XML_URL";
+        public static string xsdURL = "RAW_XSD_URL";
 
         public static void Main(string[] args)
         {
             string res = Verification(xmlURL, xsdURL);
             Console.WriteLine(res);
+
             res = Verification(xmlErrorURL, xsdURL);
             Console.WriteLine(res);
+
             res = Xml2Json(xmlURL);
             Console.WriteLine(res);
-
         }
+
         public static string Verification(string xmlURL, string xsdURL)
         {
-            try
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", xsdURL);
+
+            XmlReaderSettings sets = new XmlReaderSettings();
+            sets.ValidationType = ValidationType.Schema;
+            sets.Schemas = schemas;
+
+            List<string> errors = new List<string>();
+
+            sets.ValidationEventHandler += (sender, e) =>
             {
-                XmlSchemaSet schemas = new XmlSchemaSet();
-                schemas.Add("", xsdUrl);
-                XmlReaderSettings sets = new XmlReaderSettings();
-                sets.schemas = schemas;
-                sets.ValidationType = ValidationType.Schema;
-                List<string> errors = new List<string>();
-                sets.ValidationEventHandler += (sender, e) =>
-                {
-                    errors.add(e.Message);
-                };
-                using (XmlReader reader = XmlReader.Create(xmlURL, sets))
-                {
-                    while (reader.Read()) { }
-                }
-                return errors.Count == 0 ? "No errors are found" :
-                string.Join("\n", errors);
-            }
-            catch (Exception ex)
+                errors.Add(e.Message);
+            };
+
+            using (XmlReader reader = XmlReader.Create(xmlURL, sets))
             {
-                return ex.Message;
+                while (reader.Read()) { }
             }
 
-
+            return errors.Count == 0 ? "No errors are found" : string.Join("\n", errors);
         }
-        public static string Xml12Json(string xmlUrl)
+
+        public static string Xml2Json(string xmlUrl)
         {
-            try
-            {
-                XmlDocument doc = new XmlDocument();
-                doc.Load(xmlUrl);
-                string jsontext = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented, true);
-                return jsontext;
-            }
-            catch (Exception e)
-            {
-                return e.Message;
-            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(xmlUrl);
+
+            string jsonText = JsonConvert.SerializeXmlNode(doc);
+            return jsonText;
         }
     }
-
 }
